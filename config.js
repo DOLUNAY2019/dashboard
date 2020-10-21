cf3a48242ed38e88aa0485aa967862fc55b8c8f7
DOLUNAY <59256896+DOLUNAY2019@users.noreply.github.com>
2020-10-21 18:18:32 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+  token: "NzYwMTE3MDM3ODgzOTgxODU0.X3HYKA.SXFDxlUkH5GyuX5vsnDLUTbMREk",
+
+  dashboard: {
+    oauthSecret: "5kdbxya7-OpMDB0XpnqcJdklMyFnkogM", //client secret giriyorsunuz
+    callbackURL: "https://yildirim-web.glitch.me/callback", // botunuza redicet ekleyin bu şekilde bunuda buraya yazın ve ayrıca guilds ve botu seçin
+    sessionSecret: "g",
+    domain: "https://yildirim-web.glitch.me", // kendi linkini gireceksiniz
+    port: 9000
+  }
+};
+
+module.exports = config;
